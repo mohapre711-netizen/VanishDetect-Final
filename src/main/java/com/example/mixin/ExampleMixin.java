b2bcde5f4074dcd9b6af0f98d03ea3a2f88b436e
mohapre711-netizen <mohapre711@gmail.com>
2026-05-29 05:03:58 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,8 +1,8 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.effect.MobEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,23 +11,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class ExampleMixin {
 
-    // إلغاء تأثير الاختفاء للاعبين في شاشتك وجعلهم مرئيين
+    // إلغاء تأثير الاختفاء للاعبين في شاشتك
     @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
     private void cancelPlayerInvisibility(CallbackInfoReturnable<Boolean> cir) {
-        if ((Object) this instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) (Object) this;
-            if (player.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+        if ((Object) this instanceof Player) {
+            Player player = (Player) (Object) this;
+            if (player.hasEffect(MobEffects.INVISIBILITY)) {
                 cir.setReturnValue(false);
             }
         }
     }
 
-    // تشغيل الهالة المضيئة (Glowing Outline) حول اللاعب المخفي لتمييزه
+    // إجبار اللعبة على رسم هالة مضيئة (Glowing) حول اللاعب المخفي
     @Inject(method = "hasGlowingOutline", at = @At("HEAD"), cancellable = true)
     private void forceGlowingOnInvisiblePlayers(CallbackInfoReturnable<Boolean> cir) {
-        if ((Object) this instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) (Object) this;
-            if (player.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+        if ((Object) this instanceof Player) {
+            Player player = (Player) (Object) this;
+            if (player.hasEffect(MobEffects.INVISIBILITY)) {
                 cir.setReturnValue(true);
             }
         }
