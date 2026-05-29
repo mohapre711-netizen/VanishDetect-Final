5f7b606de164ed889c1b4258b04bfc2d0c6edfa0
mohapre711-netizen <mohapre711@gmail.com>
2026-05-29 04:46:26 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,35 @@
-package com.example.mixin;
+package net.fabricmc.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(Entity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    // إلغاء تأثير الاختفاء للاعبين في شاشتك وجعلهم مرئيين
+    @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
+    private void cancelPlayerInvisibility(CallbackInfoReturnable<Boolean> cir) {
+        if ((Object) this instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) (Object) this;
+            if (player.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+
+    // تشغيل الهالة المضيئة (Glowing Outline) حول اللاعب المخفي لتمييزه
+    @Inject(method = "hasGlowingOutline", at = @At("HEAD"), cancellable = true)
+    private void forceGlowingOnInvisiblePlayers(CallbackInfoReturnable<Boolean> cir) {
+        if ((Object) this instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) (Object) this;
+            if (player.hasStatusEffect(StatusEffects.INVISIBILITY)) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
 }
